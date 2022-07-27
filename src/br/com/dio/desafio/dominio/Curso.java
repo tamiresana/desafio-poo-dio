e1b11132c3c5a66705e2c1f16a59fb08e8a15038
tamiresanap <tamiresanap@outlook.com>
2022-07-26 21:48:39 -0300

    Mentoria e Curso

---
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,43 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso {
+    private String titulo;
+    private String desricao;
+    private int cargaHoraria;
+
+    public Curso() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDesricao() {
+        return desricao;
+    }
+
+    public void setDesricao(String desricao) {
+        this.desricao = desricao;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + titulo + '\'' +
+                ", desricao='" + desricao + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+}
